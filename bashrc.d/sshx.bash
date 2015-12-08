132dae756501f677d6dcf8d11ab725a2d9fc9fc0
Bernard Roesler <bernard.roesler@gmail.com>
2015-12-08 16:29:54 -0500

    Add check in sshx() to prompt when nesting ssh sessions.

--- bashrc.d/sshx.bash
+++ bashrc.d/sshx.bash
@@ -1,6 +1,17 @@
 # ssh to babylon x
 function sshx() 
 {
+  if [ -n "$SSH_TTY" ]; then
+    read -p "Warning: Aready in SSH session. Ok to nest? [yn] >" > choice
+    case $choice in
+      y)
+        ssh -X d26725q@babylon$1.thayer.dartmouth.edu
+        ;;
+      *)
+        return 0
+    esac
+  else
     ssh -X d26725q@babylon$1.thayer.dartmouth.edu
+  fi
 }
 export -f sshx
