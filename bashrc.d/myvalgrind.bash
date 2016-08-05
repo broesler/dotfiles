33cd11757f61c22d6dae848dbb5c1743f0ea9c51
Bernard Roesler <bernard.roesler@gmail.com>
2016-08-05 11:55:25 -0400

    Move valgrind options to valgrindrc
    Former-commit-id: 83f15a2e904e3b1eb997a164379d5f67c295d36f [formerly 78f15df9a4428ae77edfcf3a2771b70bdbf6b817] [formerly cf70d668b1d53d562c7f9163f35eaeafd1b5baad [formerly 4916c6c9f64330c11ced2a76db0275f4a780d52b]]
    Former-commit-id: 6b9e53a83cbfdc5bc6dc653d584aa753864b02f0 [formerly 1fafa3076c586600459aed3efb778adb9ff64c4d]
    Former-commit-id: 567a6d4843aceb6e46bd2ad34e2f3b04b7a7f9ba

--- bashrc.d/myvalgrind.bash
+++
@@ -1,9 +0,0 @@
-# Standard valgrind options
-function myvalgrind() 
-{ 
-  options='--leak-check=full --show-leak-kinds=all --track-origins=yes'
-  options+='--dsymutil=yes --log-file=valout'
-
-  command valgrind "$options" -v "$@"
-}
-export -f myvalgrind
