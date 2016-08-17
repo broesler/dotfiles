e49b35078267a29e6a3169954558b2b786408423
Bernard Roesler <bernard.roesler@gmail.com>
2016-08-17 19:15:30 -0400

    Include rlwrap for matlab.

--- bashrc.d/matlab.bash
+++ bashrc.d/matlab.bash
@@ -1,8 +1,11 @@
-# Open Matlab on my mac
+# Open MATLAB on my mac using bash readline!!
 if [[ "$OSTYPE" == "darwin"* ]]; then
   function matlab()
   {
-    /Applications/MATLAB_R2015b.app/bin/matlab "$@"
+    # /Applications/MATLAB_R2015b.app/bin/matlab "$@"
+    rlwrap -a dummy -c -m dummy \
+      -H $HOME/.matlab/R2015b/history.m \
+      /Applications/MATLAB_R2015b.app/bin/matlab -nosplash -nodesktop "$@"
   }
   export -f matlab
 fi
