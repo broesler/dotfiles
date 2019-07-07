58722ebe0bb264f82e398e0f4837fd6a4e3fc33d
Bernard Roesler <bernard.roesler@gmail.com>
2019-07-06 22:07:39 -0400

    Clean aliases.
    Former-commit-id: 9e88fba395902c12b7c58fb72cdc5af8b80385eb [formerly 09264d2c8a36e6479d035af017f91732bd6bc290] [formerly 3bfd30fadd930338b38509457f2f14f447fd5ac0 [formerly bfd6e88d3969f0aaff0f3842c80f693a1165a7d9]]
    Former-commit-id: b8b2156785f5c39c49993544c54c357fbb49d260 [formerly 732b7de44c1b8f794a4d7d6c31a441b10fb45a37]
    Former-commit-id: 2ecc1142b0cf81c96c1b13774128aa6a38f41c27

--- bash_profile
+++ bash_profile
@@ -7,16 +7,15 @@
 #  Description: Loads for all login shells. Sets path variable and others
 #===============================================================================
 
-host=$(hostname -s) # i.e. 't1854', 'babylon', 'polaris'
+host=$(hostname -s)  # i.e. 't1854', 'babylon', 'polaris'
 
-# Only on my Macbook, assumed OK on other machines
+# Only on my Macbook, assumed ok on other machines
 case "$host" in
 t1854)
-    # Set architecture flags for compilers
-    export ARCHFLAGS="-arch x86_64"
+    export ARCHFLAGS="-arch x86_64"  # set architecture flags for compilers
+    export TERM='screen-256color'    # required by tmux
 
-    # Default PATH
-    export DEFAULT_PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+    export DEFAULT_PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
     export PATH="$DEFAULT_PATH:/opt/X11/bin"
 
     # Add coreutils from homebrew, i.e. $(brew --prefix coreutils)
@@ -27,19 +26,14 @@ t1854)
         export MANPATH="/usr/local/opt/${name}/libexec/gnuman:$MANPATH"
     done
 
-    export PATH="$PATH:/Library/TeX/texbin"  # LaTeX path
-
-    # Add MATLAB binaries to path (for mlint, etc.)
-    export PATH="$PATH:/Applications/MATLAB_R2018a.app/bin"
-
-    # ensure tmux uses colors
-    export TERM='screen-256color'
+    export PATH="$PATH:/Library/TeX/texbin"                  # LaTeX path
+    export PATH="$PATH:/Applications/MATLAB_R2018a.app/bin"  # for mlint, etc.
     ;;
 esac
 
 # Add my local files
 export PATH="$PATH:$HOME/bin"
-export SAVE_PATH=$PATH  # keep the default path around
+export SAVE_PATH=$PATH  # keep the default path for reference
 
 # default less options (-A fails on older versions)
 export LESS=-AXFirsx8g
@@ -53,11 +47,10 @@ if [ -r "$HOME/.oldpwd" ]; then
     pushd -n "$OLDPWD" > /dev/null
 fi
 
-#------------------------------------------------------------------------------
-#       Load .bashrc file for interactive shells
-#------------------------------------------------------------------------------
+# Load .bashrc file for interactive shells
 if [ -f ~/.bashrc ]; then
     source ~/.bashrc
 fi
 #==============================================================================
 #==============================================================================
+# vim: ft=sh syntax=sh
--- bashrc.d/aliases.bash
+++ bashrc.d/aliases.bash
@@ -4,8 +4,6 @@
 #  Created: 12/04/14
 #   Author: Bernie Roesler
 # 
-#  Last Modified: 09/14/2016, 12:49
-# 
 #  Description: Contains aliases and simple functions for use with the bash
 #       shell. This file is only loaded with ~/.bashrc for interactive shells.
 # 
@@ -15,7 +13,6 @@ alias ep='vim ~/.bash_profile'               # edit profile (loaded with login)
 alias erc='vim ~/.bashrc'                    # edit rc file (loaded with bash)
 alias ea='vim ~/.bashrc.d/aliases.bash'      # edit aliases (loaded after rc)
 alias rp='source ~/.bash_profile'            # reload profile
-alias rrc='source ~/.bashrc'                 # reload JUST rc file (more common)
 
 #-------------------------------------------------------------------------------
 #       FOLDER AND APPLICATION SHORTCUTS
@@ -24,65 +21,20 @@ host=$(hostname -s) # i.e. 't1854', 'babylon', 'polaris'
 
 case "$host" in
 t1854)  # my MBP
-    # Quick directories
-    alias cs174='cd ~/Documents/School/14F-15X/cs174_machine_learning/'
-    alias es105='cd ~/Documents/School/13F-14X/Engs_105_Numerical_PDEs_1/hw/'
-    alias es145='cd ~/Documents/School/13F-14X/Engs_145_Modern_Control_Theory/'
-    alias es145='cd ~/Documents/School/15F-16X/engg149_system_identification/hw/'
-    alias es148='cd ~/Documents/School/14F-15X/engg148_structural_mechanics/'
-    alias es150='cd ~/Documents/School/13F-14X/Engs_150/'
-    alias es205='cd ~/Documents/School/14F-15X/engs205_numerical_pdes_II/labs/'
-    alias es91='cd ~/Documents/School/11F-12S/Engs_91/labs/'
-    alias mycv='cd ~/Documents/CV_Resume/CV/cv_latex/'
-    alias myresume='cd ~/Documents/CV_Resume/Resume/resume_latex/'
-    alias sty='cd /Users/bernardroesler/Library/texmf/tex/latex/'
-
-    # Apps
-    alias mlint='/Applications/MATLAB_R2018a.app/bin/maci64/mlint'
-    alias xfoil='/Applications/Xfoil.app/Contents/Resources/xfoil'
-
     # Matlab with rlwrap (use vi commands in Matlab!)
     function matlabrl()
     {
-        # $PATH includes: /Applications/MATLAB_R2016b.app/bin/
+        # $PATH includes: /Applications/MATLAB_R2018b.app/bin/
         rlwrap -a dummy -c -m dummy \
             -H $HOME/.matlab/R2018a/history.m \
             matlab -nosplash -nodesktop "$@"
     }
     ;;
-
-# Add case for work PC here...
 esac
-
-# cs[56]0 work on Mac and ThayerFS (Linux)
-function cs50()
-{
-    base="$HOME/Documents/School/cs50_software_design"
-    if [ $# -gt 0 ]; then
-        cd "$base/labs/lab$1/"
-    else
-        cd "$base"
-    fi
-}
-
-function cs60()
-{
-    base="$HOME/Documents/School/cs60_computer_networks"
-    if [ $# -gt 0 ]; then
-        cd "$base/labs/lab$1/"
-    else
-        cd "$base"
-    fi
-}
 
 #-------------------------------------------------------------------------------
 #       UTILITIES
 #-------------------------------------------------------------------------------
-# Color ag like grep:
-agcolors=" --color-path '0;49;38;5;5'"
-agcolors+=" --color-line-number '0;49;32'"
-agcolors+=" --color-match '1;49;38;5;9'"
-
 # gfortran options
 gfopts=' -cpp -Wall -pedantic -std=f95'
 gfopts+=' -fbounds-check -ffree-line-length-0 -fbacktrace -fall-intrinsics'
@@ -95,7 +47,6 @@ alias h='history | command less +G'
 alias j='jobs -l'
 alias lt='tree -C'
 alias mkdir='mkdir -p'
-alias myag="ag $agcolors"
 alias mygcc='gcc-8 -Wall -pedantic -std=c99'
 alias mygfortran="gfortran $gfopts"
 alias show_path='echo $PATH | tr -s ":" "\n"'
@@ -107,8 +58,8 @@ alias ta='type -a'
 [ -e "$DIR_COLORS" ] || DIR_COLORS=""
 
 # Set ls with colors
-eval "$(dircolors -b ~/.dircolors)"     # set custom colors file
-alias lc='ls -Ghlp --color=auto'        # Linux ls options
+eval "$(dircolors -b $DIR_COLORS)"  # set custom colors file
+alias lc='ls -Ghlp --color=auto'    # gnu-ls options
 
 # Show hidden files only
 alias lcd='lc -d .*'
@@ -118,31 +69,6 @@ alias ..='cd ..'
 alias .2='cd ../../'
 alias .3='cd ../../../'
 alias .4='cd ../../../../'
-
-#------------------------------------------------------------------------------- 
-#        Notes
-#-------------------------------------------------------------------------------
-# Colors:   
-    #   attribute = single digit
-    #       1 = bold
-    #       2 = dim
-    #       3 = italics? 
-    #       4 = underline
-    #       5 = blink
-    #       7 = invert fg/bg
-    #       9 = hidden (i.e. for passwords) 
-    #   foreground = 3? normal,  9? bright
-    #   background = 4? normal, 10? bright
-    #       9 = default
-    #       0 = black
-    #       1 = red
-    #       2 = green
-    #       3 = yellow
-    #       4 = blue
-    #       5 = magenta
-    #       6 = cyan
-    #       7 = light grey
-    #       8;5;[0-256] = 256 ANSI colors, i.e. '38;5;[0-256]'
 
 #===============================================================================
 #===============================================================================
